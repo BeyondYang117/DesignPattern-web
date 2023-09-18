61e97a9a3bd95239e765387ea62725623be52f08
caozp <caozp@servyou.com.cn>
2023-09-18 09:32:45 +0800

    删除过时的

--- src/main/java/com/dp/decorative/LongBlack.java
+++
@@ -1,8 +0,0 @@
-package com.dp.decorative;
-
-public class LongBlack extends Coffee{
-    public LongBlack(){
-        setDes("longblack");
-        setPrice(5.0f);
-    }
-}
--- src/main/java/com/dp/decorative/ShortBlack.java
+++
@@ -1,8 +0,0 @@
-package com.dp.decorative;
-
-public class ShortBlack extends Coffee {
-    public ShortBlack(){
-        setDes("shortblack");
-        setPrice(4.0f);
-    }
-}
